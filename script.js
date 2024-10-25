7aa6567f3cd15bbdbba0fdb813ddc0b1f4e53b6f
Élaff Ramos <valelaff10@gmail.com>
2024-10-25 10:23:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-function endereco(){
-
+function endereco() {
     const ceps = document.getElementById('cep').value
     const resultado = document.querySelector('.resultado')
 
-    fetch(`https://viacep.com.br/ws/${ceps}/json/`) //APi
-    .then(res => res.json())
-    .then(data => {
-        resultado.innerHTML = `<br>Rua: <b>${data.logradouro }</b>
-                              <br>Estado: <b>${data.estado }</b>
-                              <br>Cidade: <b>${data.localidade }</b>
-                              <br>Bairro: <b>${data.bairro }</b>
-                              <br>DD: <b>${data.ddd }</b>`
-    })
-    .catch(e => {
-        if(ceps.length !== 8){
-            resultado.innerHTML = '<br> Digite um CEP válido!'
-        } else {
-            resultado.innerHTML = `<br> O CEP <strong>${ceps}</strong>, não foi encontrado!`
-        }
-            
-    })
+    // Validação do comprimento do CEP
+    if (ceps.length !== 8 || !/^\d+$/.test(ceps)) {
+        resultado.innerHTML = '<br> Digite um <b>CEP</b> válido!'
+        return;
+    }
+
+    resultado.innerHTML = '<br> Carregando...' // Mensagem de carregamento
+
+    fetch(`https://viacep.com.br/ws/${ceps}/json/`) // Use backticks
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Erro na resposta da rede')
+            }
+            return res.json()
+        })
+        .then(data => {
+            if (data.erro) {
+                throw new Error('CEP não encontrado')
+            }
+            resultado.innerHTML = `
+                <br>Logradouro: <b>${data.logradouro}</b>
+                <br>Bairro: <b>${data.bairro}</b>
+                <br>Cidade: <b>${data.localidade}</b>
+                <br>Estado: <b>${data.uf}</b>
+                <br>DD: <b>(${data.ddd})</b>
+            `
+        })
+        .catch(e => {
+            resultado.innerHTML = `<br> O CEP <strong>${ceps}</strong> não foi encontrado!`
+        })
 }
